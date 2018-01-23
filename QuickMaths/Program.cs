e5f8b694e0d54a42cef2561b2465c9221a3c97f4
Ivo Meixner <ivo.meixner@se7en.cz>
2018-01-24 00:22:14 +0100

    Add project files.

---
+++ QuickMaths/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuickMaths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
